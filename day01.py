48b9c6ed24a5ac992164cb9f7924bebc9b764e2b
5loika <allen@newcombs.net>
2023-12-03 01:24:32 +0000

    Minor updates to Day01

--- day01.py
+++ day01.py
@@ -1,39 +1,43 @@
 #!/bin/python3
+""" Advent of Code 2023 -- Day 01"""
 
 import sys
-from aocd import data
-from aocd import submit
 import re
-testdata = """1abc2
+import aocd
+
+TESTDATA1 = """1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet
 """
-# testdata = """two1nine
-# eightwothree
-# abcone2threexyz
-# xtwone3four
-# 4nineeightseven2
-# zoneight234
-# 7pqrstsixteen
-# """
-test1 = 142
-test2 = 281
+TESTDATA2 = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"""
+TEST1 = 142
+TEST2 = 281
 
 
 def part1(rawdata):
+    """Code to solve part 1 of the puzzle"""
     tot = 0
     lines = rawdata.splitlines()
     for line in lines:
-        x = re.findall("\d", line)
-        tot += 10*int(x[0])+int(x[-1])
+        x = re.findall("\\d", line)
+        tot += 10 * int(x[0]) + int(x[-1])
     return tot
 
 
 def part2(rawdata):
+    """Code to solve part 2 of the puzzle"""
     lines = rawdata.splitlines()
     tot = 0
     for line in lines:
+        # Messy, but it works!
         line = line.replace("one", "o1e")
         line = line.replace("two", "t2o")
         line = line.replace("three", "t3e")
@@ -43,8 +47,8 @@ def part2(rawdata):
         line = line.replace("seven", "s7n")
         line = line.replace("eight", "e8t")
         line = line.replace("nine", "n9e")
-        x = re.findall("\d", line)
-        tot += 10*int(x[0])+int(x[-1])
+        x = re.findall("\\d", line)
+        tot += 10 * int(x[0]) + int(x[-1])
     return tot
 
 
@@ -52,27 +56,27 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         if sys.argv[1] == "test":
             if sys.argv[2] == "a":
-                result1 = part1(testdata)
-                if test1 == result1:
+                result1 = part1(TESTDATA1)
+                if TEST1 == result1:
                     print("Test 1 - Pass:", result1)
                 else:
                     print("Test 1 - Fail:", result1)
             elif sys.argv[2] == "b":
-                result2 = part2(testdata)
-                if test2 == result2:
+                result2 = part2(TESTDATA2)
+                if TEST2 == result2:
                     print("Test 2 - Pass:", result2)
                 else:
                     print("Test 2 - Fail:", result2)
         elif sys.argv[1] == "submit":
             if sys.argv[2] == "a":
-                submit(part1(data), part="a")
+                aocd.submit(part1(aocd.data), part="a")
             elif sys.argv[2] == "b":
-                submit(part2(data), part="b")
+                aocd.submit(part2(aocd.data), part="b")
         elif sys.argv[1] == "run":
             if sys.argv[2] == "a":
-                print("Part 1: ", part1(data))
+                print("Part 1: ", part1(aocd.data))
             elif sys.argv[2] == "b":
-                print("Part 2: ", part2(data))
+                print("Part 2: ", part2(aocd.data))
     else:
-        print("Part 1: ", part1(data))
-        print("Part 2: ", part2(data))
+        print("Part 1: ", part1(aocd.data))
+        print("Part 2: ", part2(aocd.data))

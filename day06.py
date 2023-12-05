5410cc57dff866901f99ab6da946a6c16dcc928b
5loika <allen@newcombs.net>
2023-12-05 22:30:36 +0000

    Template for Day 06

---
+++ day06.py
@@ -0,0 +1,59 @@
+#!/bin/python3
+""" Advent of Code 2023 -- Day 06"""
+
+import sys
+import aocd
+
+TESTDATA1 = TESTDATA2 = """
+"""
+
+TEST1 = None
+TEST2 = None
+
+
+def part1(rawdata):
+    """Code to solve part 1 of the puzzle"""
+    result = None
+    lines = rawdata.splitlines()
+    for line in lines:
+        continue
+    return result
+
+
+def part2(rawdata):
+    """Code to solve part 2 of the puzzle"""
+    result = None
+    lines = rawdata.splitlines()
+    for line in lines:
+        continue
+    return result
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "test":
+            if sys.argv[2] == "a":
+                result1 = part1(TESTDATA1)
+                if TEST1 == result1:
+                    print("Test 1 - Pass:", result1)
+                else:
+                    print("Test 1 - Fail:", result1)
+            elif sys.argv[2] == "b":
+                result2 = part2(TESTDATA2)
+                if TEST2 == result2:
+                    print("Test 2 - Pass:", result2)
+                else:
+                    print("Test 2 - Fail:", result2)
+        elif sys.argv[1] == "submit":
+            if sys.argv[2] == "a":
+                aocd.submit(part1(aocd.data), part="a")
+            elif sys.argv[2] == "b":
+                aocd.submit(part2(aocd.data), part="b")
+        elif sys.argv[1] == "run":
+            if sys.argv[2] == "a":
+                print("Part 1: ", part1(aocd.data))
+            elif sys.argv[2] == "b":
+                print("Part 2: ", part2(aocd.data))
+    else:
+        print("Part 1: ", part1(aocd.data))
+        print("Part 2: ", part2(aocd.data))

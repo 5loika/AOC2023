b11601574bcd8bdea51e2f455cf2536d4ff561e0
5loika <allenn@gmail.com>
2023-12-10 21:05:47 +0000

    Day 10 - Part 2 - Using Shapely Library (still slow)

--- day10.py
+++ day10.py
@@ -3,17 +3,30 @@
 
 import sys
 import aocd
+from shapely.geometry import Point, Polygon
 
-TESTDATA1 = TESTDATA2 = """-L|F7
+TESTDATA1 = """-L|F7
 7S-7|
 L|7||
 -L-J|
 L|-JF
 """
 
+TESTDATA2 = """.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+"""
 
 TEST1 = 4
-TEST2 = None
+TEST2 = 8
+
 
 # | is a vertical pipe connecting north and south.
 # - is a horizontal pipe connecting east and west.
@@ -55,13 +68,13 @@ def part1(rawdata):
             if map[r][c] == "S":
                 start = (r, c)
     path.append(start)
-    print("Start at:", start)
+    # print("Start at:", start)
     a = None
     b = None
     # check North:
     if start[0] > 1:
         if map[start[0] - 1][start[1]] in "|7F":
-            print("Up is connected")
+            # print("Up is connected")
             if a == None:
                 a = (start[0] - 1, start[1])
             elif b == None:
@@ -69,9 +82,9 @@ def part1(rawdata):
             else:
                 return None
     # check East:
-    elif start[1] + 1 < cols:
+    if start[1] + 1 < cols:
         if map[start[0]][start[1] + 1] in "-J7":
-            print("Right is connected")
+            # print("Right is connected")
             if a == None:
                 a = (start[0], start[1] + 1)
             elif b == None:
@@ -79,9 +92,9 @@ def part1(rawdata):
             else:
                 return None
     # check South:
-    elif start[0] + 1 < rows:
+    if start[0] + 1 < rows:
         if map[start[0] + 1][start[1]] in "|LJ":
-            print("Down is connected")
+            # print("Down is connected")
             if a == None:
                 a = (start[0] + 1, start[1])
             elif b == None:
@@ -89,9 +102,9 @@ def part1(rawdata):
             else:
                 return None
     # check West:
-    elif start[1] - 1 > 1:
+    if start[1] - 1 > 1:
         if map[start[0]][start[1] - 1] in "-LF":
-            print("Left is connected")
+            # print("Left is connected")
             if a == None:
                 a = (start[0], start[1] - 1)
             elif b == None:
@@ -99,7 +112,6 @@ def part1(rawdata):
             else:
                 return None
     path.append(a)
-    dest = b
     curr = a
     while True:
         if map[curr[0]][curr[1]] == "|":  # North/South
@@ -131,25 +143,164 @@ def part1(rawdata):
             print("Nothing matched, Input error?")  # Unknown
             return None
         if a not in path:
-            print("Add:", a, "to path", len(path))
+            # print("Add:", a, "to path", len(path))
             path.append(a)
             curr = a
         elif b not in path:
-            print("Add: ", b, "to path", len(path))
+            # print("Add: ", b, "to path", len(path))
             path.append(b)
             curr = b
         else:
-            print("Both a and b already on path")
+            # print("Both a and b already on path")
             break
-    print(len(path))
     return len(path) // 2 + len(path) % 2
 
 
 def part2(rawdata):
     """Code to solve part 2 of the puzzle"""
+    map = []
+    path = []
+    start = None
+    lines = rawdata.splitlines()
+    for l in lines:
+        map.append([x for x in l])
+    rows = len(map)
+    for r in range(rows):
+        cols = len(map[r])
+        for c in range(cols):
+            if map[r][c] == "S":
+                start = (r, c)
+    path.append(start)
+    # print("Start at:", start[0],start[1])
+    # print("Rows = ",rows,"\tCols =",cols)
+    a = None
+    b = None
+    # check North:
+    if start[0] >= 1:
+        # print("Check North of Start: ",start[0]-1,start[1])
+        if map[start[0] - 1][start[1]] in "|7F":
+            # print("North is connected")
+            if a == None:
+                a = (start[0] - 1, start[1])
+            elif b == None:
+                b = (start[0] - 1, start[1])
+            else:
+                return None
+    # check East:
+    if start[1] + 1 < cols:
+        # print("Check East of Start")
+        if map[start[0]][start[1] + 1] in "-J7":
+            # print("East is connected")
+            if a == None:
+                a = (start[0], start[1] + 1)
+            elif b == None:
+                b = (start[0], start[1] + 1)
+            else:
+                return None
+    # check South:
+    if start[0] + 1 < rows:
+        # print("Check South of Start")
+        if map[start[0] + 1][start[1]] in "|LJ":
+            # print("South is connected")
+            if a == None:
+                a = (start[0] + 1, start[1])
+            elif b == None:
+                b = (start[0] + 1, start[1])
+            else:
+                return None
+    # check West:
+    if start[1] - 1 >= 1:
+        # print("Check West of Start")
+        if map[start[0]][start[1] - 1] in "-LF":
+            # print("West is connected")
+            if a == None:
+                a = (start[0], start[1] - 1)
+            elif b == None:
+                b = (start[0], start[1] - 1)
+            else:
+                return None
+    path.append(a)
+    curr = a
+    # print(curr)
+    while True:
+        if map[curr[0]][curr[1]] == "|":  # North/South
+            a = north(curr)
+            b = south(curr)
+        elif map[curr[0]][curr[1]] == "-":  # East/West
+            a = east(curr)
+            b = west(curr)
+        elif map[curr[0]][curr[1]] == "L":  # North/East
+            a = north(curr)
+            b = east(curr)
+        elif map[curr[0]][curr[1]] == "J":  # North/West
+            a = north(curr)
+            b = west(curr)
+        elif map[curr[0]][curr[1]] == "7":  # South/West
+            a = south(curr)
+            b = west(curr)
+        elif map[curr[0]][curr[1]] == "F":  # South/East
+            a = south(curr)
+            b = east(curr)
+        elif map[curr[0]][curr[1]] == ".":  # Ground
+            print("Why is there a . on path?")
+            return None
+        elif map[curr[0]][curr[1]] == "S":  # Start
+            print(curr)
+            print("Why did I get back to S?")
+            break
+        else:
+            print("Nothing matched, Input error?")  # Unknown
+            return None
+        if a not in path:
+            # print("Add:", a, "to path", len(path))
+            path.append(a)
+            curr = a
+        elif b not in path:
+            # print("Add: ", b, "to path", len(path))
+            path.append(b)
+            curr = b
+        else:
+            # print("Both a and b already on path")
+            break    
 
+    mpoly = Polygon(path)
+    count = 0
+    for r in range(len(map)):
+        for c in range(len(map[0])):
+            p = Point(r,c)
+            if mpoly.contains(p):
+                count += 1
 
-    return None
+    # change = True
+    # while change:
+    #     change = False
+    #     for i in range(len(map)):
+    #         for j in range(len(map[0])):
+    #             if (i,j) not in path and map[i][j] != 'O':
+    #                 if i == 0 or j == 0 or i == len(map)-1 or j == len(map[0])-1:
+    #                     change = True
+    #                     map[i][j] = 'O'
+    #                 elif map[i-1][j] == 'O' or map[i+1][j] == 'O' or map[i][j-1]=='O' or map[i][j+1] == 'O':
+    #                     change = True
+    #                     map[i][j] = 'O'
+    #                 else:
+    #                     c = 0
+    #                     for n in range(j+1):
+    #                         if map[i][n] in '|LFJ7':
+    #                             c += 1
+    #                     print("Point:", i, j,"\tCount =",c)
+    #                     if  c % 2 == 0:
+    #                         map[i][j] = 'O'
+    #                         change = True
+    # count = 0
+    # for r in range(len(map)):
+    #     for c in range(len(map[0])):
+    #         if (r,c) not in path and map[r][c] != 'O':
+    #             count += 1
+    #         print(map[r][c],end=' ')
+    #     print()
+                       
+    return count
 
 
 if __name__ == "__main__":
